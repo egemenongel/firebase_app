6603a659ba0cbf30c8ce1ccd6cc5a265974bc7bb
egemenongel <egemenongel@hotmail.com>
2022-06-21 22:33:47 +0300

    Update reset password

--- lib/features/views/auth/reset_password/viewmodel/reset_password_viewmodel.dart
+++ lib/features/views/auth/reset_password/viewmodel/reset_password_viewmodel.dart
@@ -1,10 +1,26 @@
 import 'package:firebase_app/core/base/viewmodel/base_viewmodel.dart';
+import 'package:firebase_app/features/views/auth/reset_password/service/reset_password_service.dart';
 import 'package:flutter/cupertino.dart';
+import 'package:flutter_i18n/flutter_i18n.dart';
+import 'package:fluttertoast/fluttertoast.dart';
 
 class ResetPasswordViewmodel extends BaseViewmodel {
   ResetPasswordViewmodel(super.context);
 
   TextEditingController emailController = TextEditingController();
 
-  void resetPassword() async {}
+  Future resetPassword() async {
+    await ResetPasswordService()
+        .resetPassword(emailController.text)
+        .then((response) {
+      if (response is String) {
+        Fluttertoast.showToast(
+            msg: FlutterI18n.translate(context, 'common.errors.$response'));
+      } else {
+        Fluttertoast.showToast(
+            msg: FlutterI18n.translate(
+                context, 'common.messages.reset-password-email'));
+      }
+    });
+  }
 }
